fedce68699c4ee316d4eb41baae3e6d97aae8572
Le Duc Duy <me@duy.kr>
2012-12-11 19:30:07 +0900

    Use builtin routes implementation instead

--- lib/rails-routes-js-utils.rb
+++ lib/rails-routes-js-utils.rb
@@ -1,40 +1,17 @@
 require "rails-routes-js-utils/version"
 require "rails-routes-js-utils/engine"
-
+require "regex"
 module Rails
   module Routes
     module Js
       module Utils
         def self.generate
           all_routes = ENV['CONTROLLER'] ? Rails.Application.routes.select { |route| route.defaults[:controller] == ENV['CONTROLLER'] } : IMuaSam::Application.routes
-          param_split = /\(?\.?:[^\/\(\)]*\)?/
-          param_replace = '([^\/\(\)]*)'
-
           all_routes.routes.collect do |route|
-            reqs = route.requirements.empty? ? "{}" : route.requirements.to_json
-
-            this_spec = route.path.spec().to_s
-            static_part = this_spec.split(param_split)
-            dynamic_part = this_spec.scan(param_split)
-            dynamic_part.slice!(-1,1)
-            compiled_match = ['^']
-            compiled_replace = ["function() { return ''"]
-            for i in 0..[static_part.length,dynamic_part.length].max
-              if (static_part[i] && static_part[i].length)
-                compiled_match << static_part[i]
-                compiled_replace << "+'#{static_part[i]}'"
-              end
-              if (dynamic_part[i] && dynamic_part[i].length)
-                compiled_replace << "+arguments[#{i}]"
-                compiled_match << param_replace
-              end
-            end
-            compiled_match << '\/?$'
-            compiled_regex = Regexp.new(compiled_match.join());
-            compiled_replace << ";}";
-
+            compiled_regex = route.path.to_regexp.to_javascript
+            reqs = route.defaults.merge(parts: route.parts)
             if route.name
-              "addRouteToEnv({name: '#{route.name}', path: #{compiled_regex.inspect} , reqs: #{reqs}, replace: #{compiled_replace.join()}});"
+              "addRouteToEnv({name: '#{route.name}', path: #{compiled_regex} , reqs: #{reqs}});"
             end
           end.join("\n");
         end
